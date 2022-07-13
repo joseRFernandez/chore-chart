b7e8d7f34efec80bfeddbec70a8ab56bc6945056
Jose_Fernandez <jfernand84@gmail.com>
2022-07-13 09:21:57 -0400

    creating a storeCreate branch

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore } from "vuex";
+import member from "./modules/member";
 
 export default createStore({
   state: {},
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: { member },
 });
--- src/views/profile/TheProfilePage.vue
+++ src/views/profile/TheProfilePage.vue
@@ -2,93 +2,86 @@
   <div class="the-profile-page">
     <form class="form box profile-form">
       <ProfilePicSelector />
-      <div class="field">
-        <label class="label">Username</label>
-        <div class="control has-icons-left has-icons-right">
-          <input
-            class="input is-rounded"
-            type="text"
-            placeholder="Enter UserName"
-          />
-          <span class="icon is-small is-left">
-            <i class="fas fa-user"></i>
-          </span>
-          <!-- <span class="icon is-small is-right"> // to be added dynamically
+      <div class="container field-section">
+        <div class="field">
+          <label class="label">Username</label>
+          <div class="control has-icons-left has-icons-right">
+            <input
+              class="input is-rounded"
+              type="text"
+              placeholder="Enter UserName"
+              v-model="userName"
+            />
+            <span class="icon is-small is-left">
+              <i class="fas fa-user"></i>
+            </span>
+            <!-- <span class="icon is-small is-right"> // to be added dynamically
           <i class="fas fa-check"></i>
         </span> -->
+          </div>
+          <!-- <p class="help is-success">This username is available</p> dynamic add -->
         </div>
-        <!-- <p class="help is-success">This username is available</p> dynamic add -->
-      </div>
 
-      <div class="field">
-        <label class="label">Password</label>
-        <div class="control">
-          <input
-            class="input is-rounded"
-            type="password"
-            placeholder="Input Password"
-          />
+        <div class="field">
+          <label class="label">Password</label>
+          <div class="control">
+            <input
+              class="input is-rounded"
+              type="password"
+              placeholder="Input Password"
+            />
+          </div>
         </div>
-      </div>
 
-      <div class="field">
-        <label class="label">First Name</label>
-        <div class="control">
-          <input
-            class="input is-rounded"
-            type="text"
-            placeholder="Input Name"
-          />
+        <div class="field">
+          <label class="label">First Name</label>
+          <div class="control">
+            <input
+              class="input is-rounded"
+              type="text"
+              placeholder="Input Name"
+            />
+          </div>
         </div>
-      </div>
-      <div class="field">
-        <label class="label">(Optional) Middle Name</label>
-        <div class="control">
-          <input
-            class="input is-rounded"
-            type="text"
-            placeholder="Input Middle Name"
-          />
+        <div class="field">
+          <label class="label">Last Name</label>
+          <div class="control">
+            <input
+              class="input is-rounded"
+              type="text"
+              placeholder="Input Last Name"
+              value="Fernandez"
+            />
+          </div>
         </div>
-      </div>
-      <div class="field">
-        <label class="label">Last Name</label>
-        <div class="control">
-          <input
-            class="input is-rounded"
-            type="text"
-            placeholder="Input Last Name"
-            value="Fernandez"
-          />
-        </div>
-      </div>
 
-      <div class="field">
-        <label class="label">Email</label>
-        <div class="control has-icons-left has-icons-right">
-          <input
-            class="input is-rounded"
-            type="email"
-            placeholder="Email input"
-          />
-          <span class="icon is-small is-left">
-            <i class="fas fa-envelope"></i>
-          </span>
-          <!-- <span class="icon is-small is-right"> dynamic class
+        <div class="field">
+          <label class="label">Email</label>
+          <div class="control has-icons-left has-icons-right">
+            <input
+              class="input is-rounded"
+              type="email"
+              placeholder="Email input"
+            />
+            <span class="icon is-small is-left">
+              <i class="fas fa-envelope"></i>
+            </span>
+            <!-- <span class="icon is-small is-right"> dynamic class
           <i class="fas fa-exclamation-triangle"></i>
         </span> -->
+          </div>
+          <!-- <p class="help is-danger">This email is invalid</p> dynamic add -->
         </div>
-        <!-- <p class="help is-danger">This email is invalid</p> dynamic add -->
-      </div>
 
-      <div class="field is-align-self-center">
-        <label class="label">Role</label>
-        <div class="control">
-          <div class="select is-rounded">
-            <select>
-              <option>Child</option>
-              <option>Parent</option>
-            </select>
+        <div class="field single-role-select">
+          <label class="label">Role</label>
+          <div class="control">
+            <div class="select is-rounded">
+              <select>
+                <option>Child</option>
+                <option>Parent</option>
+              </select>
+            </div>
           </div>
         </div>
       </div>
@@ -98,7 +91,9 @@
           <button class="button is-link is-light">Cancel</button>
         </div>
         <div class="control">
-          <button class="button is-link">Submit</button>
+          <button class="button is-link" @click.prevent="testSwal">
+            Submit
+          </button>
         </div>
       </div>
     </form>
@@ -108,8 +103,18 @@
 import ProfilePicSelector from "./ProfilePicSelector";
 export default {
   name: "TheProfilePage",
+  data() {
+    return {
+      userName: "",
+    };
+  },
   components: {
     ProfilePicSelector,
+  },
+  methods: {
+    testSwal() {
+      this.$swal(this.userName);
+    },
   },
 };
 </script>
