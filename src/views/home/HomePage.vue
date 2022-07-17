3ecdd08320ca1d129ab9f112e03a9dbf20d4b1f8
Jose_Fernandez <jfernand84@gmail.com>
2022-07-17 15:37:32 -0400

    added greetpage skeleton - intend to add signup logic

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
-    <h2>Home Page Under Construction...</h2>
+    <div class="test-not-logged-in-div" v-if="!isLoggedIn">
+      <TheGreetPage />
+    </div>
+    <div class="is-logged-in" v-else>
+      <h2>Welcome!</h2>
+    </div>
   </div>
 </template>
 
 <script>
+import TheGreetPage from "./greetPage/TheGreetPage";
+
 export default {
   name: "home-page",
+  components: {
+    TheGreetPage,
+  },
+  data() {
+    return {
+      isLoggedIn: false,
+    };
+  },
 };
 </script>
