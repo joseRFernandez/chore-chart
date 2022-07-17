ea199a1b8c89f5eaeac7e815b01ec1dfe5f14f54
Jose_Fernandez <jfernand84@gmail.com>
2022-07-17 16:54:42 -0400

    [account-create] - create acount page skeleton

---
+++ src/views/auth/TheCreateAccountPage.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="create-account-page-wrapper">
+    <section class="section">
+      <div class="box">
+        <div class="field">
+          <label class="label">Username</label>
+          <div class="control">
+            <input
+              class="input"
+              type="text"
+              placeholder="e.g AlexSmith123"
+              v-model="chosenUserName"
+            />
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Email</label>
+          <div class="control">
+            <input
+              class="input"
+              type="email"
+              placeholder="e.g. alexsmith@gmail.com"
+              v-model="givenEmail"
+            />
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Password</label>
+          <div class="control">
+            <input class="input" type="password" v-model="chosenPassword" />
+          </div>
+        </div>
+      </div>
+      <div class="buttons-group">
+        <div class="buttons">
+          <button class="button is-link" @click="resetForm">Reset</button>
+          <button class="button is-primary">Create</button>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "the-create-account-page",
+  data() {
+    return {
+      chosenUserName: "",
+      givenEmail: "",
+      chosenPassword: "",
+    };
+  },
+  methods: {
+    resetForm() {
+      this.chosenUserName = "";
+      this.givenEmail = "";
+      this.chosenPassword = "";
+    },
+  },
+};
+</script>
--- src/views/home/greetPage/TheGreetPage.vue
+++ src/views/home/greetPage/TheGreetPage.vue
@@ -1,28 +1,18 @@
 <template>
   <div class="the-greet-page-wrapper">
     <greet-banner></greet-banner>
-    <section class="hero is-halfheight greet-page-button-wrapper">
-      <div class="hero-body is-flex is-flex-direction-column">
-        <div class="greet-page-buttons">
-          <button class="button is-info is-outlined">Create Account</button>
-          <button class="button is-info" @click="betaLogIn">Log In</button>
-        </div>
-      </div>
-    </section>
+    <greet-page-buttons></greet-page-buttons>
   </div>
 </template>
 
 <script>
 import GreetBanner from "./GreetBanner";
+import GreetPageButtons from "./GreetPageButtons";
 export default {
   name: "the-greet-page",
   components: {
     GreetBanner,
-  },
-  methods: {
-    betaLogIn() {
-      this.$swal("Take me to Log In Page!");
-    },
+    GreetPageButtons,
   },
 };
 </script>
